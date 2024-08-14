c3b4975353978410e62b84a103e43cc2a159c7cc
Deepali Singh <th.deepalisingh@gmail.com>
2024-08-14 21:30:11 +0530

    setup done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { GrChatOption } from "react-icons/gr";
+import './App.css'
+
+
+// https://react-icons.github.io/react-icons/icons/gr/
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold underline gap-2 items-center justify-center flex">
+        Hello world
+        <GrChatOption size='45px' className="text-yellow-700" />
+
+      </h1>
+    </>
+  )
+}
+
+export default App
